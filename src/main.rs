57951680bd18eb6b5f00f0a7370ffcaf3390f226
Amit Kumar <amit.kumar@atherenergy.com>
2020-02-14 13:54:41 +0530

    Working

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+use avro_rs::{Codec, Reader, Schema, Writer, from_value, types::Record};
+use failure::Error;
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Test {
+    a: i64,
+    b: String,
+}
+
+fn main() -> Result<(), Error> {
+    let raw_schema = r#"
+        {
+            "type": "record",
+            "name": "test",
+            "fields": [
+                {"name": "a", "type": "long", "default": 42},
+                {"name": "b", "type": "string"}
+            ]
+        }
+    "#;
+
+    let schema = Schema::parse_str(raw_schema)?;
+
+    println!("{:?}", schema);
+
+    let mut writer = Writer::with_codec(&schema, Vec::new(), Codec::Deflate);
+
+    let mut record = Record::new(writer.schema()).unwrap();
+    record.put("a", 27i64);
+    record.put("b", "foo");
+
+    writer.append(record)?;
+
+    let test = Test {
+        a: 27,
+        b: "foo".to_owned(),
+    };
+
+    writer.append_ser(test)?;
+
+    writer.flush()?;
+
+    let input = writer.into_inner();
+    let reader = Reader::with_schema(&schema, &input[..])?;
+
+    for record in reader {
+        println!("{:?}", from_value::<Test>(&record?));
+    }
+    Ok(())
+}
