a3124cd68e8af059ad94aa7c68a54a10a55f322b
Amit Kumar <amit.kumar@atherenergy.com>
2020-04-28 15:46:05 +0530

    a ray of hope.

--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,25 @@ use std::collections::BTreeMap;
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Test {
-    a: String,
-    b: String,
+    a: Option<String>,
+    b: Option<String>,
     c: Option<String>,
 }
 
 fn main() -> Result<(), Error> {
     let filename = "av.avsc";
-    let contents = fs::read_to_string(filename).
-        expect("No Rad");
-    println!("contents are {:?}", contents);
+    // let contents = fs::read_to_string(filename).
+    //     expect("No Rad");
+    // println!("contents are {:?}", contents);
 
     let raw_schema = r#"
         {
             "type": "record",
             "name": "test",
             "fields": [
-                {"name": "a", "type": "string"},
-                {"name": "b", "type": "string"},
-                {"name": "c",  "type": ["null", "string"]}
+                {"name": "a", "type": ["null", "string"], "default": "null"},
+                {"name": "b", "type": ["null", "string"], "default": "null"},
+                {"name": "c",  "type": ["null", "string"], "default": "null"}
             ]
         }
     "#;
@@ -35,9 +35,10 @@ fn main() -> Result<(), Error> {
     let mut writer = Writer::with_codec(&schema, Vec::new(), Codec::Deflate);
 
     let mut record = Record::new(writer.schema()).unwrap();
-    let mut check : BTreeMap<String, String> = BTreeMap::new();
-    check.insert("a".to_owned(), "100".to_owned());
-    check.insert("b".to_owned(), "200".to_owned());
+    let mut check : BTreeMap<String, Option<String>> = BTreeMap::new();
+    check.insert("a".to_owned(), Some("100".to_owned()));
+    check.insert("b".to_owned(), Some("200".to_owned()));
+    check.insert("c".to_owned(), None);
     for (key, value) in check.iter(){
         record.put(key, value.clone());
     }
@@ -62,3 +63,60 @@ fn main() -> Result<(), Error> {
     }
     Ok(())
 }
+
+#[test]
+    fn test_default() -> Result<(), Error> {
+
+        #[derive(Debug, Serialize, Deserialize)]
+        struct Test {
+            a: String,
+            b: String,
+            c: Option<String>,
+        }
+
+        let raw_schema = r#"
+        {
+            "type": "record",
+            "name": "test",
+            "fields": [
+                {"name": "a", "type": "string"},
+                {"name": "b", "type": "string"}
+            ]
+        }
+        "#;
+
+        let raw_schema_r = r#"
+        {
+            "type": "record",
+            "name": "test",
+            "fields": [
+                {"name": "a", "type": "string"},
+                {"name": "b", "type": "string"},
+                {"name": "c",  "type": ["null", "string"], "default": "null"}
+            ]
+        }"#;
+
+        let schema = Schema::parse_str(raw_schema)?;
+        let schema_r = Schema::parse_str(raw_schema_r)?;
+
+        println!("==============");
+        println!("{:?}", schema);
+
+        let mut writer = Writer::with_codec(&schema, Vec::new(), Codec::Deflate);
+
+        let mut record = Record::new(writer.schema()).unwrap();
+        record.put("a", "antonio".to_owned());
+        record.put("b", "antonio".to_owned());
+
+        writer.append(record)?;
+
+        writer.flush()?;
+
+        let input = writer.into_inner();
+        let reader = Reader::with_schema(&schema_r, &input[..])?;
+
+        for record in reader {
+            println!("{:?}", from_value::<Test>(&record?));
+        }
+        Ok(())
+     }
