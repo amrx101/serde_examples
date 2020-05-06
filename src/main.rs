a269c246985f1affe49bc9c9b3182e197e04e59f
Amit Kumar <amit.kumar@atherenergy.com>
2020-05-06 17:16:35 +0530

    only_string.

--- src/main.rs
+++ src/main.rs
@@ -3,113 +3,72 @@ use failure::Error;
 use serde::{Serialize, Deserialize};
 use std::fs::File;
 use std::io::prelude::*;
-use std::collections::BTreeMap;
+use std::collections::{HashMap, BTreeMap};
+use serde_json;
 
-#[derive(Debug, Deserialize, Serialize)]
-struct Test {
-    a: Option<String>,
-    b: Option<String>,
-    c: Option<String>,
-    d: Option<f64>,
+#[derive(Debug, Serialize, Deserialize)]
+pub struct G2Data {
+    #[serde(default)]
+    can_id: Option<i32>,
+    #[serde(default)]
+    value: Option<String>,
+    #[serde(default)]
+    key: Option<String>,
+    #[serde(default)]
+    timestamp: Option<i32>,
+    #[serde(default)]
+    start_timestamp: Option<String>,
+    #[serde(default)]
+    end_timestamp: Option<String>,
+    #[serde(default)]
+    release_name: Option<String>,
+    #[serde(default)]
+    bms_version: Option<String>,
+    #[serde(default)]
+    charger_version: Option<String>,
+    #[serde(default)]
+    sim_ccid: Option<String>,
+    #[serde(default)]
+    sim_cimi: Option<String>,
+    #[serde(default)]
+    mender_artifact_ver: Option<String>,
+    #[serde(default)]
+    mcu_version: Option<String>,
+    #[serde(default)]
+    vin: Option<String>,
+    #[serde(default)]
+    bike_type: Option<String>,
+    #[serde(default)]
+    motor_version: Option<String>,
+    #[serde(default)]
+    system_boot_time: Option<String>,
+    #[serde(default)]
+    mode: Option<i32>,
+    #[serde(default)]
+    error_code: Option<String>,
+    #[serde(default)]
+    is_valid: Option<i32>,
+    #[serde(default)]
+    ACC_X_MPS2: Option<f64>,
+    #[serde(default)]
+    ACC_Y_MPS2: Option<f64>,
+    #[serde(default)]
+    ACC_Z_MPS2: Option<f64>,
+    #[serde(default)]
+    GYR_X_DEG: Option<f64>,
+    #[serde(default)]
+    GYR_Y_DEG: Option<f64>,
+    #[serde(default)]
+    GYR_Z_DEG: Option<f64>,
 }
 
-fn main() -> Result<(), Error> {
-    let filename = "av.avsc";
-    // let contents = fs::read_to_string(filename).
-    //     expect("No Rad");
-    // println!("contents are {:?}", contents);
-
-    let mut file = File::open("/home/amit/rust_samples/avr_ser/av.avsc")?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-    let schema = Schema::parse_str(&contents)?;
-
-
-    let mut writer = Writer::with_codec(&schema, Vec::new(), Codec::Deflate);
-
-    let mut record = Record::new(writer.schema()).unwrap();
-    let mut check : BTreeMap<String, Option<String>> = BTreeMap::new();
-    check.insert("a".to_owned(), Some("100".to_owned()));
-    check.insert("b".to_owned(), Some("200".to_owned()));
-    check.insert("c".to_owned(), None);
-    for (key, value) in check.iter(){
-        record.put(key, value.clone());
-    }
-
-   writer.append(record)?;
-
-    // let test = Test {
-    //     a: "27".to_owned(),
-    //     b: "foo".to_owned(),
-    //     c: None,
-    // };
-
-    // writer.append_ser(test)?;
-
-    writer.flush()?;
-
-    let input = writer.into_inner();
-    let reader = Reader::with_schema(&schema, &input[..])?;
-
-    for record in reader {
-        println!("{:?}", from_value::<Test>(&record?));
-    }
-    Ok(())
-}
-
-#[test]
-    fn test_default() -> Result<(), Error> {
-
-        #[derive(Debug, Serialize, Deserialize)]
-        struct Test {
-            a: String,
-            b: String,
-            c: Option<String>,
-        }
-
-        let raw_schema = r#"
+fn main() {
+    println!("Hello world");
+    let data = r#"
         {
-            "type": "record",
-            "name": "test",
-            "fields": [
-                {"name": "a", "type": "string"},
-                {"name": "b", "type": "string"}
-            ]
-        }
-        "#;
-
-        let raw_schema_r = r#"
-        {
-            "type": "record",
-            "name": "test",
-            "fields": [
-                {"name": "a", "type": "string"},
-                {"name": "b", "type": "string"},
-                {"name": "c",  "type": ["null", "string"], "default": "null"}
-            ]
+            "can_id": 11
         }"#;
+    let v: G2Data = serde_json::from_str(data).unwrap();
+    println!("v is {:?}", v);
 
-        let schema = Schema::parse_str(raw_schema)?;
-        let schema_r = Schema::parse_str(raw_schema_r)?;
-
-        println!("==============");
-        println!("{:?}", schema);
-
-        let mut writer = Writer::with_codec(&schema, Vec::new(), Codec::Deflate);
-
-        let mut record = Record::new(writer.schema()).unwrap();
-        record.put("a", "antonio".to_owned());
-        record.put("b", "antonio".to_owned());
-
-        writer.append(record)?;
-
-        writer.flush()?;
-
-        let input = writer.into_inner();
-        let reader = Reader::with_schema(&schema_r, &input[..])?;
-
-        for record in reader {
-            println!("{:?}", from_value::<Test>(&record?));
-        }
-        Ok(())
-     }
+}
