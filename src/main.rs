e1b87168790e569c9d01bec28b6c7399e07dff17
amrx101 <amrx101@gmail.com>
2020-12-02 10:55:17 +0530

    WIP

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use std::str::FromStr;
 use std::time::Instant;
 use derive_more::From;
 use std::io::{self, prelude::*, BufReader};
+// use avrow::{from_value as fv, Codec as cc, Reader as rr, Schema as sc, Writer as w, Record as Rr};
 
 
 use serde::de::{self, Deserializer};
@@ -81,6 +82,16 @@ pub struct G2Data {
     GYR_Y_DEG: Option<f32>,
     #[serde(default, deserialize_with="from_str_optional")]
     GYR_Z_DEG: Option<f32>,
+    #[serde(default)]
+    LAT_DEG: Option<String>,
+    #[serde(default)]
+    LON_DEG: Option<String>,
+    #[serde(default)]
+    ALT_M: Option<String>,
+    #[serde(default)]
+    Accuracy:Option<String>,
+    #[serde(default)]
+    incognito_mode: Option<String>
 
 }
 
@@ -141,6 +152,16 @@ pub struct G2DataRes {
     GYR_Y_DEG: Option<f32>,
     #[serde(default)]
     GYR_Z_DEG: Option<f32>,
+    #[serde(default)]
+    LAT_DEG: Option<String>,
+    #[serde(default)]
+    LON_DEG: Option<String>,
+    #[serde(default)]
+    ALT_M: Option<String>,
+    #[serde(default)]
+    Accuracy:Option<String>,
+    #[serde(default)]
+    incognito_mode:Option<String>
 
 }
 
@@ -186,7 +207,33 @@ fn get_imu_data() -> G2Data {
 // https://play.rust-lang.org/?version=stable&mode=debug&edition=2018&code=use%20serde%3A%3ADeserialize%3B%0Ause%20serde_json%3A%3Ajson%3B%0Ause%20std%3A%3Acollections%3A%3ABTreeMap%3B%0A%0A%23%5Bderive(Deserialize%2C%20Debug)%5D%0Astruct%20User%20%7B%0A%20%20%20%20fingerprint%3A%20Option%3CString%3E%2C%0A%20%20%20%20location%3A%20String%2C%0A%7D%0A%0Afn%20main()%20%7B%0A%20%20%20%20let%20mut%20m%3A%20BTreeMap%3CString%2C%20String%3E%20%3D%20BTreeMap%3A%3Anew()%3B%0A%20%20%20%20m.insert(%22fingerprint%22.to_owned()%2C%20%22aa%22.to_owned())%3B%0A%20%20%20%20m.insert(%22location%22.to_owned()%2C%20%22aa%22.to_owned())%3B%0A%20%20%20%20%2F%2F%20The%20type%20of%20%60j%60%20is%20%60serde_json%3A%3AValue%60%0A%20%20%20%20let%20j%20%3D%20json!(%7B%0A%20%20%20%20%20%20%20%20%2F%2F%20%22fingerprint%22%3A%20%220xF9BA143B95FF6D82%22%2C%0A%20%20%20%20%20%20%20%20%22location%22%3A%20%22Menlo%20Park%2C%20CA%22%0A%20%20%20%20%7D)%3B%0A%20%20%20%20%0A%20%20%20%20let%20d%20%3D%20json!(m)%3B%0A%0A%20%20%20%20let%20u%3A%20User%20%3D%20serde_json%3A%3Afrom_value(j).unwrap()%3B%0A%20%20%20%20println!(%22%7B%3A%23%3F%7D%22%2C%20u)%3B%0A%20%20%20%20let%20du%3A%20User%20%3D%20serde_json%3A%3Afrom_value(d).unwrap()%3B%0A%20%20%20%20println!(%22%7B%3A%23%3F%7D%22%2C%20du)%3B%0A%7D
 // https://play.rust-lang.org/?version=stable&mode=debug&edition=2018&gist=f265edc1b9e5fd4485a83da40fd01785
 
+fn get_digital_twin_data()-> G2Data{
+    let mot_data = r#"{
+        "key": "MOT_NPres_RPM",
+        "value": "121.12",
+        "timestamp": "1589441481.885"
+    }"#;
+    serde_json::from_str(mot_data).unwrap()
+}
 
+fn get_dt_soc_data() -> Vec<G2Data>{
+    let mut res:Vec<G2Data> = Vec::new();
+    let cell_1 = r#"{
+        "key": "BMS_Cell_Temp_2",
+        "value": "1232.32",
+        "timestamp": "1589441481.885"
+    }"#;
+    let v1 = serde_json::from_str(cell_1).unwrap();
+    res.push(v1);
+    let cell_2 = r#"{
+        "key": "BMS_Cell_Temp_3",
+        "value": "1232.32",
+        "timestamp": "1589441481.885"
+    }"#;
+    let v2 = serde_json::from_str(cell_2).unwrap();
+    res.push(v2);
+    res
+}
 
 
 fn tt() -> Result<Vec<u8>, MyError> {
@@ -204,13 +251,20 @@ fn tt() -> Result<Vec<u8>, MyError> {
     let now = Instant::now();
     // create 10 records based on the string and ask codec to keep it for future.
     for n in 1..10 {
-        let v: G2Data = get_can_data();
-        println!("v={:?}", v);
+        let v: G2Data = get_digital_twin_data();
         match codec_writer.append_ser(v) {
             Ok(f) => f,
             Err(e) => return Err(MyError::SerdeSerializer(e.to_string()))
         };
     }
+    let soc_data = get_dt_soc_data();
+    for data in soc_data {
+        match codec_writer.append_ser(data) {
+            Ok(f) => f,
+            Err(e) => return Err(MyError::SerdeSerializer(e.to_string()))
+        };
+    }
+
 
     // do the serialization and compression.
     match codec_writer.flush() {
@@ -221,23 +275,7 @@ fn tt() -> Result<Vec<u8>, MyError> {
 
     // serilalized bytes
     let ec = codec_writer.into_inner();
-    println!("{:?}", ec);
-
-   // now we will attempt to deserialize the bytes.
-    let reader = Reader::with_schema(&schema, &ec[..]).unwrap();
-    let mut vec_d: Vec<G2DataRes> = Vec::new();
-    for record in reader {
-        let mut d = match from_value::<G2DataRes>(&record.unwrap()){
-            Ok(v) => v,
-            Err(e) => return Err(MyError::SerdeSerializer(e.to_string()))
-        };
-        vec_d.push(d);
-    }
-
-    println!("{:?}", vec_d);
-    println!("Total_items_DESERIALIZED={:?}", vec_d.len());
     Ok(ec)
-
 }
 
 fn compress_deflate(uncompressed_buffer: &[u8]) -> Vec<u8> {
